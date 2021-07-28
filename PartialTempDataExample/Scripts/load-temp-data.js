992db196efd5a897b4151ddd5138f7c31f38b57e
Jason Burton <jason.burton@redstitchdigital.com>
2021-07-28 12:06:56 -0500

    separated out the script for loading the partial view via js

---
+++ PartialTempDataExample/Scripts/load-temp-data.js
@@ -0,0 +1,9 @@
+﻿document.querySelector("#load").addEventListener("click", () => {
+  fetch("/Home/ShowPartial?searchTerm=NewTest")
+    .then(response => {
+      return response.text();
+    })
+    .then(result => {
+      document.querySelector("#partial").innerHTML = result;
+    });
+});
