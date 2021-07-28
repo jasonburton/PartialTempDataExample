fe0b09e1d146e9dbc88be9f298e68f6c63679133
Jason Burton <jason.burton@redstitchdigital.com>
2021-07-28 11:54:13 -0500

    Add project files.

---
+++ PartialTempDataExample/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartialTempDataExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (TempData.ContainsKey("searchTerm"))
+            {
+                ViewBag.searchTerm = TempData["searchTerm"].ToString();
+            }
+            return View();
+        }
+        public PartialViewResult ShowPartial(string searchTerm)
+        {
+            return PartialView("ShowPartial", searchTerm);
+        }
+        public ActionResult PartialTest(string searchTerm = "")
+        {
+
+            TempData["searchTerm"] = searchTerm;
+            
+            return RedirectToAction("Index", "Home");
+
+        }
+       
+    }
+}
